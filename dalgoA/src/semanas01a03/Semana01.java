938ad6b26dc936dfb0367b23b0988f89e0965bd1
Daniel R Barrero R <barreroroserodr@gmail.com>
2025-01-24 11:16:25 -0500

    added algorithms for the first couple of classes.

--- dalgoA/src/semanas01a03/Semana01.java
+++ dalgoA/src/semanas01a03/Semana01.java
@@ -21,5 +21,82 @@ public class Semana01 {
 		}
 		return result;
 	}
+	
+	public static boolean naiveSearch(int[][] mm, int y) {
+		int height = mm.length;
+		int width = mm[0].length;
+		for(int row = 0; row < height; row++) {
+			for(int col = 0; col < width; col++) {
+				if(mm[row][col] == y) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+	public static boolean searchInSorted(int[][] mm, int y) {
+		int height = mm.length;
+		int width = mm[0].length;
+		int row = 0;
+		int col = width-1;
+		while(row < height && col >= 0) {
+			if(y == mm[row][col]) {
+				return true;
+			}
+			else if(y > mm[row][col]) {
+				row++;
+			}
+			else {
+				col--;
+			}
+		}
+		return false;
+	}
+	
+	public static void insertionSort(int[] aa) {
+		int n = aa.length;
+		for(int i=1; i<n; i++) {
+			int x = aa[i];
+			int j = i-1;
+			while(j>=0 && x<aa[j]) {
+				aa[j+1] = aa[j];
+				j--;
+							}
+			aa[j+1] = x;
+		}
+	}
+	
+	public static float bcSqrt(int n, int steps) {
+		if(steps <= 0 || n <= 0) {
+			return n;
+		}
+		float lower = 0f;
+		float upper = n;
+		float ans = 0f;
+		boolean found = false;
+		while(steps > 0 && !found) {
+			ans = (upper + lower)/2;
+			if(ans*ans == n) {
+				found = true;
+			}
+			else if(ans*ans > n) {
+				upper = ans;
+			}
+			else {
+				lower = ans;
+			}
+			steps--;
+		}
+		return ans;
+	}
+	
+	public static void main(String[] args) {
+		float a = 3/7f;
+		
+		System.out.println(a);
+		System.out.println();
+		System.out.println(bcSqrt(9, 29));
+	}
 
 }
