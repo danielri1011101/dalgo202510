0b4d967fd0891a25d261a82058a3f2c72c03b9f8
Daniel R Barrero R <barreroroserodr@gmail.com>
2025-03-31 08:57:31 -0500

    did Bellman-Ford.

--- dalgoA/src/semanas09a10/DijkstraBellmanFord.java
+++ dalgoA/src/semanas09a10/DijkstraBellmanFord.java
@@ -29,7 +29,43 @@ public class DijkstraBellmanFord {
 	}
 	
 	public static int[] bellmanFord(DiGraph gg, int source) {
+		int[] distances = new int[gg.order];
+		fillArray(distances, Integer.MAX_VALUE);
+		distances[source] = 0;
 		
-	}
+		for(int i=1; i < gg.order; i++) {
+			for(Edge edge : gg.edges) {
+				int u = edge.source;
+				int v = edge.target;
+				int weight = edge.weight;
+				
+				if(distances[u] < Integer.MAX_VALUE && distances[u] + weight <
+						distances[v]) {
+					distances[v] = distances[u] + weight;
+				}
+			}
+		}
+		
+		// check for negative-weight cycles.
 
+		for(Edge edge : gg.edges) {
+			int u = edge.source;
+			int v = edge.target;
+			int weight = edge.weight;
+			
+			if(distances[u] < Integer.MAX_VALUE && distances[u] + weight < 
+					distances[v]) {
+				System.out.println("This digraph contains negative cycles.");
+			}
+		}
+
+		return distances;
+	}
+	
+	public static void fillArray(int[] aa, int value) {
+		for(int i=0; i < aa.length; i++) {
+			aa[i] = value;
+		}
+	}
+	
 }
