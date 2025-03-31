4ce21e1d85df89dc8a000356f7b3dc2e3227776d
Daniel R Barrero R <barreroroserodr@gmail.com>
2025-03-31 11:55:17 -0500

    Implemented naive Dijkstra, main method missing.

--- dalgoA/src/semanas09a10/DijkstraBellmanFord.java
+++ dalgoA/src/semanas09a10/DijkstraBellmanFord.java
@@ -28,6 +28,51 @@ public class DijkstraBellmanFord {
 
 	}
 	
+	/**
+	 * Sub-optimal but very readable.
+	 * @param gg
+	 * @param source
+	 * @return
+	 */
+	public static int[] dijkstra(DiGraph gg, int source) {
+		int n = gg.order;
+		int[] distances = new int[n];
+		fillArray(distances, Integer.MAX_VALUE);
+		distances[source] = 0;
+		boolean[] visited = new boolean[n];
+		
+		for(int i=0; i < n; i++) {
+			
+			// find closest node
+			int u = -1;
+			int minDist = Integer.MAX_VALUE;
+			for(int v=0; v < n; v++) {
+				if(!visited[v] && distances[v] < minDist) {
+					minDist = distances[v];
+					u = v;
+				}
+			}
+			
+			visited[u] = true;
+			 
+			if(u==-1) {
+				break;
+			}
+			
+			for(Edge edge : gg.edges) {
+				if(edge.source == u) {
+					int v = edge.target;
+					int weight = edge.weight;
+					int newDist = distances[u] + weight;
+					if(newDist < distances[v]) {
+						distances[v] = newDist;
+					}
+				}
+			}
+		}
+		return distances;
+	}
+	
 	public static int[] bellmanFord(DiGraph gg, int source) {
 		int[] distances = new int[gg.order];
 		fillArray(distances, Integer.MAX_VALUE);
@@ -39,7 +84,8 @@ public class DijkstraBellmanFord {
 				int v = edge.target;
 				int weight = edge.weight;
 				
-				if(distances[u] < Integer.MAX_VALUE && distances[u] + weight <
+				int newDist = distances[u] + weight;
+				if(distances[u] < Integer.MAX_VALUE && newDist <
 						distances[v]) {
 					distances[v] = distances[u] + weight;
 				}
@@ -53,7 +99,8 @@ public class DijkstraBellmanFord {
 			int v = edge.target;
 			int weight = edge.weight;
 			
-			if(distances[u] < Integer.MAX_VALUE && distances[u] + weight < 
+			int newDist = distances[u] + weight;
+			if(distances[u] < Integer.MAX_VALUE && newDist < 
 					distances[v]) {
 				System.out.println("This digraph contains negative cycles.");
 			}
